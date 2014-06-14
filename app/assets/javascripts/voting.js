988a656ec336a4ae44f3267f9ffb3169556840a5
Thomas  Carpenito <thomascarpenito@Tommy.local>
2014-06-14 12:52:31 -0700

    BindListeners have been added to the voting buttons

--- app/assets/javascripts/voting.js
+++ app/assets/javascripts/voting.js
@@ -2,24 +2,44 @@ $(document).ready(function(){
 	var model = new votesModel
 	var view = new votesView
 	var controller = new votesController(view, model)
+	controller.bindListeners()
+
 })
 
 ////////////////////////Controller
 var votesController = function(view, model){
-	this.view = view //creates the buttons
+	this.view = view //creates the buttons- done
 	this.model = model // will perform the logic of voting
 
 }
 
+votesController.prototype = {
+	bindListeners: function(){
+		this.view.upVote.on("click", this.model.upVoteClip)
+		this.view.downVote.on("click", this.downVoteClip)
+
+	},
+}
 
 
 
 ////////////////////////Model
 var votesModel = function(){
 
+//Capture the user_id from the soundclip
+
 }
+votesModel.prototype = {
 
+	upVoteClip: function(){
+		// debugger
+	},
 
+	downVoteClip: function(){
+		debugger
+	}
+
+}
 
 
 
