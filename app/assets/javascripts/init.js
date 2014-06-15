75432adf075fe6a04d970a44df869f20e3fd3829
Apprentice <apprentice@dbc30.local>
2014-06-14 22:43:52 -0700

    buttons initial scaffold created in html and css

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -3,14 +3,30 @@ $(document).ready(function(){
 
 
 	$("#jquery_jplayer_1").jPlayer({
-	ready: function () {
-	$(this).jPlayer("setMedia", {
-	title: "Bubble",
-	m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
-	oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
-	});
-	},
-	swfPath: "/js",
-	supplied: "m4a, oga"
-	});
+		ready: function () {
+			$(this).jPlayer("setMedia", {
+				m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
+			});
+		},
+			supplied: "m4a"
+		});
 });
+
+
+// click on button
+// ajax request to a route you define
+// said route will return a mp3 url
+// plug that into your media player function
+
+
+// $(document).ready( init )
+
+// function init(){
+// 	var view = new View
+// 	var blueView = new BlueView
+// 	var model = new Model
+// 	var controller = new Controller( model, view, blueView )
+// 	controller.bindEventListeners()
+// }
+
+
