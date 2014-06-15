fe23f7da1f90c9eb366afd1aeafdc216229e4f43
Thomas  Carpenito <thomascarpenito@Tommy.local>
2014-06-15 00:11:34 -0700

    Brought down the master branch to push to heroku and noticed some changes that were made in previous commits (and merges) that disrupted the master branch. The index.html.erb page was not rendering the login if the current_user did not exist, but rather the navbar (changed). This caused issues with sessions not clearing. All is fixed.

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20140614214507) do
+ActiveRecord::Schema.define(version: 20140614214603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,13 +31,6 @@ ActiveRecord::Schema.define(version: 20140614214507) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "votes", force: true do |t|
-    t.integer "user_id"
-    t.integer "clip_id"
-    t.boolean "like",    default: false
-    t.boolean "visible", default: false
   end
 
 end
