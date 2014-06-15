d6e501ba77cd630b64087d240c34d5218ce0d0e4
Thomas  Carpenito <thomascarpenito@Tommy.local>
2014-06-14 20:15:17 -0700

    downloaded jplayer and swf, included in javascript tree cassette image used as the player cassette styled and positioned in the center app init.js file created to handle all document.ready loads

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,18 +1,3 @@
-$(document).ready(function(){
-  eventListeners();
-
-    $("#jquery_jplayer_1").jPlayer({
-        ready: function(event) {
-            $(this).jPlayer("setMedia", {
-                mp3: "http://www.jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3",
-                oga: "http://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg"
-            });
-        },
-        swfPath: "http://www.jplayer.org/2.1.0/js",
-        supplied: "mp3, oga"
-    });
-});
-
 function eventListeners(){
   $('#mic').on('click', showClipModal)
   $('.close-new-clip').on('click', closeClipModal)
