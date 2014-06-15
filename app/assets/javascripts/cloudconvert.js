8632f75817c3054412be748a8b3d7eff0f41471c
Thomas  Carpenito <thomascarpenito@Tommy.local>
2014-06-15 13:30:16 -0700

    On ajax success working

--- app/assets/javascripts/cloudconvert.js
+++ app/assets/javascripts/cloudconvert.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
-  $('#clip_upload').ajaxForm(myFormAction)
+  	var options = {
+  		success : returnDownloadLink,
+  		error : errorUploadingClip
+  	}
+  $('#clip_upload').ajaxForm(options)
 })
 
-function myFormAction(){
+function returnDownloadLink(){
 	$.getJSON(this.url, function(data) {
 		console.log(data['output'].url)
 	})
+}
+
+function errorUploadingClip(){
+	console.log("Yikes, we can't upload that!")
+	debugger
 }
