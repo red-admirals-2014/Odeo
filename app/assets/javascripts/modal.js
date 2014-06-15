49dfead61f79e06c36252ca90488a31ca9908710
Thomas  Carpenito <thomascarpenito@Tommy.local>
2014-06-14 19:19:24 -0700

    Lots of changes happening here. Added the jquery music player, so that works. Now able to use the jquery player on our page.
     with '#' will be ignored, and an empty message aborts the commit.

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
   eventListeners();
+
+    $("#jquery_jplayer_1").jPlayer({
+        ready: function(event) {
+            $(this).jPlayer("setMedia", {
+                mp3: "http://www.jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3",
+                oga: "http://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg"
+            });
+        },
+        swfPath: "http://www.jplayer.org/2.1.0/js",
+        supplied: "mp3, oga"
+    });
 });
 
 function eventListeners(){
