b94e70f71f9e0f8b16dbf6ae4f62a9ad4b33f6ed
Amy Foster <akfoster_2001@yahoo.com>
2014-06-14 17:54:17 -0700

    added ajax call to stop page routing to converter response, but not getting response

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -1,13 +1,20 @@
 
 class ClipsController < ApplicationController
   include HTTParty
-  
-  def index
-  end
 
-  def new
+  def index
     @response = HTTParty.post("https://api.cloudconvert.org/process?apikey=#{ENV['CLOUD_API_KEY']}&inputformat=wav&outputformat=mp3")
     puts @response['url']
   end
+
+  # def new
+  # end
+
+  # def upload
+  #   uploaded_io = params[:clip][:audio]
+  #   File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
+  #     HTTParty.post()
+  #   end
+  # end
 
 end
