79b1f6b0095fc358c3ce7b2cbf27b448d7e79197
Jason Smith & Tommy <pair+smithjason.tommyixi@devbootcamp.com>
2014-06-15 20:36:20 -0700

    Added Pending Test   - Clips     - #create, invalid params       - doesn't work when adding presence validation for clip_link for some reason.

--- spec/controllers/clips_controller_spec.rb
+++ spec/controllers/clips_controller_spec.rb
@@ -16,14 +16,20 @@ describe ClipsController do
         new_user = FactoryGirl.create(:user)
         session[:user_id] = new_user.id
         expect{
-          post :create, clip: FactoryGirl.attributes_for(:clip)
+          post :create, FactoryGirl.attributes_for(:clip)
         }.to change(Clip, :count).by(1)
       end
       it "redirects to root_path" do
         new_user = FactoryGirl.create(:user)
         session[:user_id] = new_user.id
-        post :create, clip: FactoryGirl.attributes_for(:clip)
+        post :create, FactoryGirl.attributes_for(:clip)
         expect(response).to redirect_to root_path
+      end
+    end
+
+    context "with invalid attributes" do
+      it "does not save the new clip to the database" do
+        pending "adding a validation to Clip model for clip_link presence gives an expectation to have views to render for no apparent reason"
       end
     end
   end
