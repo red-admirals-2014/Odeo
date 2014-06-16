3376271085734143e40681baf93dc07076c599c1
Tommy <null>
2014-06-16 15:50:37 -0700

    Able to vote and play the next song. Needs to be refactored

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -28,11 +28,23 @@ class ClipsController < ApplicationController
     p "These are our params"
     p params
 
+    unless params[:url] == "https://srv23.cloudconvert.org/download/i7F82KLc"
+      clip = Clip.where(clip_link: params[:url]).first
+      if params[:vote] == 'upvote'
+        vote = Vote.create(user_id: session[:user_id], clip_id: clip.id, like: true)
+        # render :text => next_song.clip_link, layout: false
+      elsif params[:vote] == 'downvote'
+        vote = Vote.create(user_id: session[:user_id], clip_id: clip.id, like: false)
+        # render :text => next_song.clip_link, layout: false
+      else
+        render :text => "https://srv23.cloudconvert.org/download/i7F82KLc", layout: false
+      end
+    end
+
     if params[:url] == "https://srv23.cloudconvert.org/download/i7F82KLc"
       potential_next_songs = Clip.where(created_at: (Time.now - 1.day)..Time.now + 1.day)
       voted_on = potential_next_songs.includes(:votes).where('votes.user_id = ?', session[:user_id]).references(:votes)
-      all_clips = Clip.all
-      possible_songs = all_clips - voted_on
+      possible_songs = potential_next_songs - voted_on
       next_song = possible_songs.sample
       p "This is the next song"
       p next_song
@@ -43,22 +55,16 @@ class ClipsController < ApplicationController
       all_clips = Clip.all
       possible_songs = all_clips - voted_on
       next_song = possible_songs.sample
-      p "This is the next song"
-      p next_song
-      clip = Clip.where(clip_link: params[:url]).first
+      if next_song
+        p "This is the next song"
+        p next_song
+        render :text => next_song.clip_link, layout: false
+    else
+    puts "you've voted on everything!"
+      # render :text => "you've exhausted all songs"
+    end
   #       potential_next_songs = Clip.where(created_at: (Time.now - 1.day)..Time.now + 1.day)
-      if params[:vote] == 'upvote'
-        vote = Vote.create(user_id: session[:user_id], clip_id: clip.id, like: true)
-        render :text => next_song.clip_link, layout: false
-      else
-        vote = Vote.create(user_id: session[:user_id], clip_id: clip.id, like: false)
-        render :text => next_song.clip_link, layout: false
-      end
-#     next_song = potential_next_songs.find  { |song| song.votes.where.not(user_id: session[:user_id])}
-#     p "This is the next song"
-#     p next_song
-  end
-
+    end
   end
 
 end
