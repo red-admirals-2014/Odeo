592e848cb33eb7b7d3e1a2b5e2dc899ab028d0c9
Apprentice <apprentice@dbc30.local>
2014-06-15 18:53:58 -0700

    attempted to trigger automatic play onload, did not work, see coaches

--- app/assets/javascripts/controller.js
+++ app/assets/javascripts/controller.js
@@ -8,12 +8,13 @@ Controller.prototype = {
 
      $('#mic').on('click', this.openModal.bind(this) );
      $('.close-new-clip').on('click', this.closeModal.bind(this) );
-     $('#cassette').on('click', this.triggerPlay.bind(this) );
+     $('.jp-play').on('click', this.triggerPlay.bind(this) );
      $('.click-vote').on('click', this.voteHandler.bind(this));
      $('#clip_upload').ajaxForm({
         success: returnDownloadLink,
         error: errorUploadingClip
      });
+
   },
 
   openModal: function(){
@@ -45,8 +46,6 @@ function returnDownloadLink(){
 
 function errorUploadingClip(){
   console.log("Yikes, we can't upload that!")
-  // difficult to test when cloud convert can basically accept
-  // all file types. We will need to set a limit for the upload.
 };
 
 function insertIntoDatabase(returnedURL){
