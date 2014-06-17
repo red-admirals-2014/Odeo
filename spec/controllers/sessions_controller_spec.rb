b37ff13784662ec1e1e906abdcb77e3eab809fc2
Tommy <null>
2014-06-16 20:26:28 -0700

    Second session test added

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,13 +3,21 @@ require 'spec_helper'
 describe SessionsController do
 
   describe "#create" do
-    it "redirects to the root URL upon creating a session" do
+
+    before (:each) do
       fake_user = User.create
       User.stub(:from_omniauth).and_return( fake_user )
-       # allow(user).to receive(:user) {"tommy"}
-       # allow(session).to receive(session[:user_id]) {1}
-      post :create, { provider: 'raorao'}
+    end
+
+    it "sets a session id upon user creation" do
+      post :create, {provider: 'no_provider'}
+      expect(session[:user_id]).to be(1)
+    end
+
+    it "redirects to the root URL upon creating a session" do
+      post :create, { provider: 'no_provider'}
       expect(response).to be_redirect
     end
+
   end
 end
