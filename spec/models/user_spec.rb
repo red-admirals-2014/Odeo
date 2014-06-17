eb71e5cc5a19c11ae098e3d33eef52d0e2311105
Amy Foster <akfoster_2001@yahoo.com>
2014-06-16 21:25:26 -0700

    added working user models, removed unused file for cloud tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,28 @@
 require 'spec_helper'
 
-describe User do
-  context "validations" do
+describe User do 
+
+  before (:each) do
+    fake_user = User.create
+    User.stub(:from_omniauth).and_return( fake_user)
   end
+
+  context "associations" do
+    it { should have_many(:clips) }
+    it { should have_many(:votes) }
+  end
+
+  it "should create a user when a new user logs in from facebook" do
+    facebook_auth = {"provider" => "facebook", "uid" => "asfd", "info" => { "email" => "test.example.com", "name" => "tester"}}
+    user = User.from_omniauth(facebook_auth)
+    expect(User.last).to eq(user)
+  end
+
+  it "should return an existing user when an existing user logs in from facebook" do
+    facebook_auth = {"provider" => "facebook", "uid" => "asfd", "info" => { "email" => "test.example.com", "name" => "tester"}}
+    user = User.from_omniauth(facebook_auth)
+    user2 = User.from_omniauth(facebook_auth)
+    expect(User.find(user.id)).to eq(user2)
+  end
+
 end
