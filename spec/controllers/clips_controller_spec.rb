1f3e4353d2377835e214f73e13b3f2af9c575a74
Tommy <null>
2014-06-16 20:42:35 -0700

    Starting on the clips controller

--- spec/controllers/clips_controller_spec.rb
+++ spec/controllers/clips_controller_spec.rb
@@ -1,25 +1,29 @@
 require 'spec_helper'
 
 describe ClipsController do
-  render_views
 
   describe "index" do
-    it  "a logged out user should be asked to sign in" do
-      pending
-      # this test is kind of obsolete
-      # get :index
-      # expect(response.body).to have_content "Sign in with Facebook"
+  render_views
+    it  "renders the login page for a new user" do
+      get :index
+      expect(response.body).to have_content "Oudio Oudio Log in with Facebook"
+    end
+    it "renders the homepage for a user already logged in" do
+      user = User.create
+      session[:user_id] = 1
+      get :index
+      expect(response.body).to have_content "Oudio Oudio Upload or record from your phone! Or Record yourself from the browser! Cancel"
     end
   end
 
   describe "POST create" do
     context "with valid attributes" do
       it "saves new clip to the database" do
-        pending
-        # new_user = FactoryGirl.create(:user)
+        # pending
+        # new_user = User.create
         # session[:user_id] = new_user.id
         # expect{
-        #   post :create, FactoryGirl.attributes_for(:clip)
+        #   post :create, new_user
         # }.to change(Clip, :count).by(1)
       end
       it "redirects to root_path" do
