18178545b61d0239ef84278e2aa79fc76ad2a416
Tommy <null>
2014-06-16 20:32:42 -0700

    All sessions-controller testing completed

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,23 +1,30 @@
 require 'spec_helper'
 
 describe SessionsController do
-
-  describe "#create" do
-
     before (:each) do
       fake_user = User.create
       User.stub(:from_omniauth).and_return( fake_user )
     end
 
+  describe "#create" do
     it "sets a session id upon user creation" do
       post :create, {provider: 'no_provider'}
       expect(session[:user_id]).to be(1)
     end
-
     it "redirects to the root URL upon creating a session" do
       post :create, { provider: 'no_provider'}
       expect(response).to be_redirect
     end
+  end
 
+  describe "#destroy" do
+    it "sets a session to nil upon logout" do
+      post :destroy
+      expect(session[:user_id]).to be(nil)
+    end
+    it "redirects to the root_url upon destruction" do
+      post :destroy
+      expect(response).to be_redirect
+    end
   end
 end
