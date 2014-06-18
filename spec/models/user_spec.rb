ac314f7c4f843c8eb48677a94426314dd6eb5f35
Amy Foster <akfoster_2001@yahoo.com>
2014-06-18 11:23:34 -0700

    added working tests for clips_controller, user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,9 @@ describe User do
   before (:each) do
     fake_user = User.create
     User.stub(:from_omniauth).and_return( fake_user)
+    facebook_auth = {"provider" => "facebook", "uid" => "asfd", "info" => { "email" => "test.example.com", "name" => "tester"}}
+    @user = User.from_omniauth(facebook_auth)
+    @user2 = User.from_omniauth(facebook_auth)
   end
 
   context "associations" do
@@ -12,17 +15,12 @@ describe User do
     it { should have_many(:votes) }
   end
 
-  it "should create a user when a new user logs in from facebook" do
-    facebook_auth = {"provider" => "facebook", "uid" => "asfd", "info" => { "email" => "test.example.com", "name" => "tester"}}
-    user = User.from_omniauth(facebook_auth)
-    expect(User.last).to eq(user)
+  it "should create a user given valid facebook information" do
+    expect(User.last).to eq(@user)
   end
 
-  it "should return an existing user when an existing user logs in from facebook" do
-    facebook_auth = {"provider" => "facebook", "uid" => "asfd", "info" => { "email" => "test.example.com", "name" => "tester"}}
-    user = User.from_omniauth(facebook_auth)
-    user2 = User.from_omniauth(facebook_auth)
-    expect(User.find(user.id)).to eq(user2)
+  it "should return an existing user given valid facebook information" do
+    expect(User.find(@user2.id)).to eq(@user)
   end
 
 end
