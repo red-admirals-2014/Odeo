a0a93841c665bda9aa104565ce4b4c585a552abb
Tommy <null>
2014-06-18 16:23:24 -0700

    Fixed css styling for iphone

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,12 +1,10 @@
 class VotesController < ApplicationController
 
 	def create
-    p "these are your params"
-    p params
     song_url = params[:url]
-    base_song_url = "https://s3-us-west-1.amazonaws.com/akfoster/users/Music/01-03-+Dirt+and+Roses.mp3"
     user_vote = params[:vote]
     clip = Clip.find_by_clip_link(song_url)
+    base_song_url = "https://s3-us-west-1.amazonaws.com/akfoster/users/Music/01-03-+Dirt+and+Roses.mp3"
 
     if song_url != base_song_url && clip != nil
       if user_vote == 'upvote'
@@ -17,7 +15,6 @@ class VotesController < ApplicationController
         render :text => "downvote", layout: false
       end
     else
-      p "Now I am here!"
       render :text => "error", layout: false
     end
 	end
--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -2,8 +2,28 @@ require 'spec_helper'
 
 describe VotesController do
   describe "create" do
-    it "doens't do anything" do
-      expect(3).to eq(3)
+
+    it "renders error when the current song matches the default base_song " do
+      post :create, {song_url: "https://s3-us-west-1.amazonaws.com/akfoster/users/Music/01-03-+Dirt+and+Roses.mp3" }
+      expect(response.body).to eq("error")
     end
+
+    it "renders error when there are no more clips to choose from" do
+      post :create, {clip: nil}
+      expect(response.body).to eq("error")
+    end
+
+    it "creates an upvote if the user_vote is equal to 'upvote' " do
+      User.create
+      @clip = Clip.create(user_id: 1, clip_link: "tommy.com")
+      post :create, {user_vote: 'upvote', user_id: 1, clip: @clip, song_url: "bob.com"}
+      expect(@clip.votes.count).to eq(1)
+
+    end
+
+    it "creates a downvote if the user_vote is equal to 'downvote' " do
+
+    end
+
   end
 end
